ecc6da65b2550a22822c00c2bc52a5961f220458
Amodoi Peter <amodoipeter@gmail.com>
2022-05-19 22:23:33 +0300

    command prompt update

--- prompt.c
+++ prompt.c
@@ -1,38 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <main.h>
+#include "shell.h"
+/**
+ * shell - Infinite loop that runs shell
+ * @ac: Arg count
+ * @av: args passed to shell at beginning of prog
+ * @env: Environment
+ * Return: Void
+ */
+void shell(int ac, char **av, char **env)
+{
+	char *line;
+	char **args;
+	int status = 1;
+	char *tmp = NULL;
+	char *er;
+	char *filename;
+	int flow;
 
-void info();
-void help();
-void greet(char *name);
-
-int main(int argc, char *argv[])
-{
-    if(argc==1)
-    {
-        info();
-    }
-    if(argc==2)
-    {
-        if(strcmp(argv[1], "-h")==0)
-        {
-            help();
-        }
-        else{
-            greet(argv[1]);
-        }
-    }
-}
-void info()
-{
-    printf("Hello, type hello -h for more information\r\n");
-}
-void help()
-{
-    printf("Type hello {name} for the best greeting\r\n"); 
-}
-void greet (char *name)
-{
-    printf("Hello there, %s\r\n", name);
+	er = "Error";
+	do {
+		prompt();
+		line = _getline();
+		args = split_line(line);
+		flow = bridge(args[0], args);
+		if (flow == 2)
+		{
+			filename = args[0];
+			args[0] = find_path(args[0], tmp, er);
+			if (args[0] == er)
+			{
+				args[0] = search_cwd(filename, er);
+				if (args[0] == filename)
+					write(1, er, 5);
+			}
+		}
+		if (args[0] != er)
+			status = execute_prog(args, line, env, flow);
+		free(line);
+		free(args);
+	} while (status);
+	if (!ac)
+		(void)ac;
+	if (!av)
+		(void)av;
+	if (!env)
+		(void)env;
 }
