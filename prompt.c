329fa02f44c36fbb8c2e69af0985fc27817b9580
Amodoi Peter <amodoipeter@gmail.com>
2022-05-19 20:57:41 +0300

    command prompt update

--- prompt.c
+++ prompt.c
@@ -2,11 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+void info();
+void help();
+void greet(char *name);
 
 int main(int argc, char *argv[])
 {
     if(argc==1)
     {
-        printf("parent program passed is %s\r\n", argv[0]);
-    };
+        info();
+    }
+    if(argc==2)
+    {
+        if(strcmp(argv[1], "-h")==0)
+        {
+            help();
+        }
+        else{
+            greet(argv[1]);
+        }
+    }
+}
+void info()
+{
+    printf("Hello, type hello -h for more information\r\n");
+}
+void help()
+{
+    printf("Type hello {name} for the best greeting\r\n"); 
+}
+void greet (char *name)
+{
+    printf("Hello there, %s\r\n", name);
 }
